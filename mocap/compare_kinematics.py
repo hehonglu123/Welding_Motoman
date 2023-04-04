d0617b21b3acf36fc490879f892f6a96e0c993dc
Eric Lu <eric565648@gmail.com>
2023-04-04 17:39:08 -0400

    add d=15

--- mocap/compare_kinematics.py
+++ mocap/compare_kinematics.py
@@ -15,7 +15,7 @@ import time
 from MocapPoseListener import *
 
 config_dir='../config/'
-robot_weld=robot_obj('MA2010_A0',def_path=config_dir+'MA2010_A0_robot_default_config.yml',tool_file_path=config_dir+'weldgun.csv',\
+robot_weld=robot_obj('MA2010_A0',def_path=config_dir+'MA2010_A0_robot_default_config.yml',tool_file_path=config_dir+'weldgun.csv',d=15,\
 pulse2deg_file_path=config_dir+'MA2010_A0_pulse2deg.csv',\
 base_marker_config_file=config_dir+'MA2010_marker_config.yaml',tool_marker_config_file=config_dir+'weldgun_marker_config.yaml')
 
