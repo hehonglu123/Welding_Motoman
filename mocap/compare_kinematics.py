8d2750d05bce713f39f8fa535c3a266ce97ef5f6
Eric Lu <eric565648@gmail.com>
2023-04-06 10:24:07 -0400

    test

--- mocap/compare_kinematics.py
+++ mocap/compare_kinematics.py
@@ -19,9 +19,11 @@ robot_weld=robot_obj('MA2010_A0',def_path=config_dir+'MA2010_A0_robot_default_co
 pulse2deg_file_path=config_dir+'MA2010_A0_pulse2deg.csv',\
 base_marker_config_file=config_dir+'MA2010_marker_config.yaml',tool_marker_config_file=config_dir+'weldgun_marker_config.yaml')
 
-test_qs = np.array([[0.,0.,0.,0.,0.,0.],[0,69,57,0,0,0],[0,-68,-68,0,0,0],[-36.6018,12.4119,-12.1251,-43.3579,-45.4297,68.1203],
-                [21.0753,-1.8803,-27.3509,13.1122,-25.1173,-25.2466]])
-# test_qs = np.array([[0,69,57,0,0,0],[0,-68,-68,0,0,0]])
+# test_qs = np.array([[0.,0.,0.,0.,0.,0.],[0,69,57,0,0,0],[0,-68,-68,0,0,0],[-36.6018,12.4119,-12.1251,-43.3579,-45.4297,68.1203],
+#                 [21.0753,-1.8803,-27.3509,13.1122,-25.1173,-25.2466]])
+test_qs = np.array([[0,69,57,0,0,0],[0,-68,-68,0,0,0]])
+# print(robot_weld.fwd(np.radians([-0.5,-68,-68,0,0,0])))
+# exit()
 
 # mocap pose listener
 mocap_url = 'rr+tcp://localhost:59823?service=optitrack_mocap'
@@ -32,7 +34,7 @@ mpl_obj.run_pose_listener()
 time.sleep(5)
 mpl_obj.stop_pose_listener()
 
-repeats_N = 20
+repeats_N = 4
 rob_speed = 15
 all_robot_ctrl_pose = []
 all_robot_mocap_pose = []
@@ -53,6 +55,9 @@ for N in range(repeats_N):
         curve_p,curve_R,timestamps = mpl_obj.get_robots_traj()
         mocap_T = Transform(curve_R[robot_weld.robot_name][-1],curve_p[robot_weld.robot_name][-1])
         all_robot_mocap_pose.append(np.append(mocap_T.p,R2q(mocap_T.R)))
+    # for i in range(len(curve_p[robot_weld.robot_name])):
+    #     print(curve_p[robot_weld.robot_name][i])
+    # exit()
 
 # move robot
 robot_client = MotionProgramExecClient(IP='192.168.1.31',ROBOT_CHOICE='RB1',pulse2deg=robot_weld.pulse2deg)
@@ -70,11 +75,11 @@ mocap_T = Transform(curve_R[robot_weld.robot_name][-1],curve_p[robot_weld.robot_
 print(encoder_T)
 print(mocap_T)
 
-# for i in range(len(all_robot_mocap_pose)):
-#     print("Test i:",i)
-#     print(all_robot_ctrl_pose[i])
-#     print(all_robot_mocap_pose[i])
+for i in range(len(all_robot_mocap_pose)):
+    print("Test i:",i)
+    print(all_robot_ctrl_pose[i])
+    print(all_robot_mocap_pose[i])
 
-np.savetxt('data/compare_results_ctrl_basefix.csv',all_robot_ctrl_pose,delimiter=',')
-np.savetxt('data/compare_results_mocap_basefix.csv',all_robot_mocap_pose,delimiter=',')
+# np.savetxt('data/compare_results_ctrl_basefix.csv',all_robot_ctrl_pose,delimiter=',')
+# np.savetxt('data/compare_results_mocap_basefix.csv',all_robot_mocap_pose,delimiter=',')
 
