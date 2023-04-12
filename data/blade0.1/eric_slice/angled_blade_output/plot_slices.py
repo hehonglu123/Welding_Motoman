71e38ae80fe92f60c9d4c6eef6dcc26357096944
hehonglu123 <hehonglu123@126.com>
2023-04-11 23:05:04 -0400

    wire callback

---
+++ data/blade0.1/eric_slice/angled_blade_output/plot_slices.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys, traceback, time, copy, glob
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from general_robotics_toolbox import *
+sys.path.append('../../../../toolbox')
+from robot_def import *
+from multi_robot import *
+from error_check import *
+from path_calc import *
+
+def main():
+	vis_step=5
+	fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+	
+
+
+	
+	for i in range(1,90):
+		print(i)
+		slice_i=np.loadtxt(str(i)+'.csv',delimiter=',',usecols=(0,1,2))
+		ax.plot3D(slice_i[::vis_step,0],slice_i[::vis_step,1],slice_i[::vis_step,2],'r.-')
+
+	
+	# ax.quiver(curve[::vis_step,0],curve[::vis_step,1],curve[::vis_step,2],curve[::vis_step,0],curve[::vis_step,1],curve[::vis_step,2],length=0.3, normalize=True)
+
+	plt.show()
+if __name__ == '__main__':
+	main()
---
+++ fronius/state_log_sub.py
@@ -0,0 +1,32 @@
+from RobotRaconteur.Client import *
+import time
+import numpy as np
+
+timestamp=[]
+voltage=[]
+current=[]
+feedrate=[]
+energy=[]
+
+def wire_cb(sub, value, ts):
+    global timestamp, voltage, current, feedrate, energy
+
+    timestamp.append(value.ts['microseconds'][0])
+    voltage.append(value.welding_voltage)
+    current.append(value.welding_current)
+    feedrate.append(value.wire_speed)
+    energy.append(value.welding_energy)
+
+
+sub=RRN.SubscribeService('rr+tcp://localhost:60823?service=welder')
+obj = sub.GetDefaultClientWait(3)      #connect, timeout=30s
+welder_state_sub=sub.SubscribeWire("welder_state")
+
+
+welder_state_sub.WireValueChanged += wire_cb
+input('press enter to quit')
+
+
+np.savetxt('weld_log.csv',np.array([timestamp,voltage,current,feedrate,energy]).T,delimiter=',', header='timestamp,voltage,current,feedrate,energy',comments='')
+
+
