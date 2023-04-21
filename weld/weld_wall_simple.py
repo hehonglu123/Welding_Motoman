449ae042dffa7fc36cd143ab08f298e93d6a3ea0
hehonglu123@126.com <hehonglu123@126.com>
2023-04-21 15:59:03 -0400

    steel blade

--- weld/weld_wall_simple.py
+++ weld/weld_wall_simple.py
@@ -9,8 +9,8 @@ robot=robot_obj('MA2010_A0',def_path='../config/MA2010_A0_robot_default_config.y
 R=np.array([[ 0.7071, -0.7071, -0.    ],
 			[-0.7071, -0.7071,  0.    ],
 			[-0.,      0.,     -1.    ]])
-p_start=np.array([1615,-860,-255])
-p_end=np.array([1615,-760,-255])
+p_start=np.array([1615,-860,-260])
+p_end=np.array([1615,-760,-260])
 q_seed=np.radians([-35.4291,56.6333,40.5194,4.5177,-52.2505,-11.6546])
 q_init=np.degrees(robot.inv(p_start,R,q_seed)[0])
 q_end=np.degrees(robot.inv(p_end,R,q_seed)[0])
@@ -36,7 +36,7 @@ layer_height=0.8
 # 	client.SetArc(False)
 # 	client.execute_motion_program()
 
-for i in range(2,4):
+for i in range(10,12):
 	if i%2==0:
 		p1=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
 		p2=p_end+np.array([0,0,2*base_layer_height+i*layer_height])
