76b4fce0f989570bc136c313c59a6d6139c76f1a
hehonglu123@126.com <hehonglu123@126.com>
2023-05-23 14:31:00 -0400

    spiral welding

--- weld/weld_spiral_cylinder.py
+++ weld/weld_spiral_cylinder.py
@@ -16,28 +16,26 @@ client=MotionProgramExecClient()
 
 mp=MotionProgram(ROBOT_CHOICE='RB1',pulse2deg=robot.pulse2deg, tool_num = 12)
 
-num_layer_start=0
-num_layer_end=1
+num_layer_start=49
+num_layer_end=50
 for layer in range(num_layer_start,num_layer_end,1):
 
 	####################DETERMINE CURVE ORDER##############################################
 	curve_sliced_js=np.loadtxt(data_dir+solution_dir+'curve_sliced_js/MA2010_js'+str(layer)+'.csv',delimiter=',')
 
-	vd=7
+	vd=6
 
-	num_points_layer=3
+	num_points_layer=5 ####minimum of 5 (2 semi-circle) required for circular spiral, otherwise 3 points not work
 
 	###find which end to start
-	if layer % 2 ==0:
-		breakpoints=np.linspace(0,len(curve_sliced_js)-1,num=num_points_layer).astype(int)
-	else:
-		breakpoints=np.linspace(len(curve_sliced_js)-1,0,num=num_points_layer).astype(int)
-
+	breakpoints=np.linspace(0,len(curve_sliced_js)-1,num=num_points_layer).astype(int)
 		
 	mp.MoveJ(np.degrees(curve_sliced_js[breakpoints[0]]), 1)
 
 	mp.setArc(True,cond_num=300)
-	mp.MoveC(np.degrees(curve_sliced_js[breakpoints[0]]),np.degrees(curve_sliced_js[breakpoints[1]]),np.degrees(curve_sliced_js[breakpoints[2]]), vd,zone=1)
-	mp.setArc(False)
+	mp.MoveC(np.degrees(curve_sliced_js[breakpoints[0]]),np.degrees(curve_sliced_js[breakpoints[1]]),np.degrees(curve_sliced_js[breakpoints[2]]), vd,zone=None)
+	mp.MoveC(np.degrees(curve_sliced_js[breakpoints[2]]),np.degrees(curve_sliced_js[breakpoints[3]]),np.degrees(curve_sliced_js[breakpoints[4]]), vd,zone=None)
+
+mp.setArc(False)
 
 client.execute_motion_program(mp)
