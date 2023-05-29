af0d093ef8a7e6db843c1409a4b20d152d145fbf
hehonglu123@126.com <hehonglu123@126.com>
2023-05-29 12:45:33 -0400

    flir test

--- FLIR/ir_vis_raw.py
+++ FLIR/ir_vis_raw.py
@@ -9,8 +9,8 @@ freq=13
 fig = plt.figure(1)
 for i in range(len(ir_recording)):
     print(np.max(ir_recording[i]),np.min(ir_recording[i]))
-    # plt.imshow(ir_recording[i], cmap='inferno', aspect='auto')
-    plt.imshow(ir_recording[i]*0.1- 273.15 , cmap='inferno', aspect='auto')
+    plt.imshow(ir_recording[i], cmap='inferno', aspect='auto')
+    # plt.imshow(ir_recording[i]*0.1- 273.15 , cmap='inferno', aspect='auto')
     plt.colorbar(format='%.2f')
     plt.pause(1/freq)
     plt.clf()
