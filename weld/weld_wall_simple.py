3e0896777e6f313b1d18491e87d74dbcb749e3ff
hehonglu123@126.com <hehonglu123@126.com>
2023-05-31 21:12:01 -0400

    al welding

--- weld/weld_wall_simple.py
+++ weld/weld_wall_simple.py
@@ -21,34 +21,34 @@ client=MotionProgramExecClient()
 base_layer_height=2
 layer_height=0.8
 
-# for i in range(1,2):
-# 	if i%2==0:
-# 		p1=p_start+np.array([0,0,i*base_layer_height])
-# 		p2=p_end+np.array([0,0,i*base_layer_height])
-# 	else:
-# 		p1=p_end+np.array([0,0,i*base_layer_height])
-# 		p2=p_start+np.array([0,0,i*base_layer_height])
-
-# 	q_init=np.degrees(robot.inv(p1,R,q_seed)[0])
-# 	q_end=np.degrees(robot.inv(p2,R,q_seed)[0])
-# 	mp.MoveJ(q_init,1,0)
-# 	mp.setArc(True,cond_num=310)
-# 	mp.MoveL(q_end,5,0)
-# 	mp.setArc(False)
-# 	client.execute_motion_program(mp)
-
-for i in range(7,8):
+for i in range(0,1):
 	if i%2==0:
-		p1=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
-		p2=p_end+np.array([0,0,2*base_layer_height+i*layer_height])
+		p1=p_start+np.array([0,0,i*base_layer_height])
+		p2=p_end+np.array([0,0,i*base_layer_height])
 	else:
-		p1=p_end+np.array([0,0,2*base_layer_height+i*layer_height])
-		p2=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
+		p1=p_end+np.array([0,0,i*base_layer_height])
+		p2=p_start+np.array([0,0,i*base_layer_height])
 
 	q_init=np.degrees(robot.inv(p1,R,q_seed)[0])
 	q_end=np.degrees(robot.inv(p2,R,q_seed)[0])
 	mp.MoveJ(q_init,1,0)
-	mp.setArc(True,cond_num=305)
-	mp.MoveL(q_end,12,0)
+	mp.setArc(True,cond_num=215)
+	mp.MoveL(q_end,5,0)
 	mp.setArc(False)
 	client.execute_motion_program(mp)
+
+# for i in range(7,8):
+# 	if i%2==0:
+# 		p1=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
+# 		p2=p_end+np.array([0,0,2*base_layer_height+i*layer_height])
+# 	else:
+# 		p1=p_end+np.array([0,0,2*base_layer_height+i*layer_height])
+# 		p2=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
+
+# 	q_init=np.degrees(robot.inv(p1,R,q_seed)[0])
+# 	q_end=np.degrees(robot.inv(p2,R,q_seed)[0])
+# 	mp.MoveJ(q_init,1,0)
+# 	mp.setArc(True,cond_num=305)
+# 	mp.MoveL(q_end,12,0)
+# 	mp.setArc(False)
+# 	client.execute_motion_program(mp)
