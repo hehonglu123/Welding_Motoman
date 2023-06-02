fc01aab6e9e36ae44e525363539de2ae3884fe99
Chen-Lung Eric Lu <eric565648@gmail.com>
2023-06-02 12:37:45 -0400

    find mti alignment problem

--- scan/scan_process/merge_scan_cont_mti.py
+++ scan/scan_process/merge_scan_cont_mti.py
@@ -14,6 +14,23 @@ import time
 from copy import deepcopy
 import colorsys
 import math
+import pickle
+
+def raw_data_filtering(single_scan, noise_filter=False):
+    single_scan=single_scan.T
+    ###delete noise centered at x=0
+    single_scan=np.delete(single_scan,np.argwhere(abs(single_scan[:,0])==0).flatten(),axis=0)
+    ###clip data within range
+    indices=np.argwhere(single_scan[:,1]>50).flatten()
+    single_scan=single_scan[indices]
+
+    if noise_filter:
+        ###filter out outlier noise
+        outlier_indices=identify_outliers2(single_scan[:,1],rolling_window=20,threshold=1e-2)
+        return np.delete(single_scan,outlier_indices,axis=0)
+    else:
+        single_scan=single_scan.T
+        return single_scan
 
 ### open3d device ###
 if o3d.core.cuda.is_available():
@@ -27,8 +44,8 @@ dtype = o3d.core.float32
 # data_dir='../../data/wall_weld_test/wall_param_1/scans/'
 # data_dir='../../data/wall_weld_test/scan_cont_newdx_1/scans/'
 # data_dir='../../data/wall_weld_test/wall_param_data_collection/path_Rz-45_Ry0_stand_off_d243_b_theta45_scan_angle-45_45_z0_35_/scans/'
-# data_dir='../../data/wall_weld_test/full_test_mti/scans/'
-data_dir='../../data/wall_weld_test/top_layer_test_mti/scans/'
+data_dir='../../data/wall_weld_test/full_test_mti/scans/'
+# data_dir='../../data/wall_weld_test/top_layer_test_mti/scans/'
 config_dir='../../config/'
 
 robot_scan=robot_obj('MA1440_A0',def_path=config_dir+'MA1440_A0_robot_default_config.yml',tool_file_path=config_dir+'mti.csv',\
@@ -43,6 +60,9 @@ turn_table.base_H = H_from_RT(turn_table.T_base_basemarker.R,turn_table.T_base_b
 T_to_base = Transform(np.eye(3),[0,0,-380])
 turn_table.base_H = np.matmul(turn_table.base_H,H_from_RT(T_to_base.R,T_to_base.p))
 
+# print(robot_scan.fwd(np.zeros(6)))
+# exit()
+
 scan_js_exe = np.loadtxt(data_dir+'scan_js_exe.csv',delimiter=",", dtype=np.float64)
 rob_stamps = np.loadtxt(data_dir+'robot_stamps.csv',delimiter=",", dtype=np.float64)
 rob_stamps=rob_stamps-rob_stamps[0]
@@ -55,6 +75,7 @@ voxel_size=0.05
 
 pcd_combined = None
 scan_N = len(rob_stamps) ## total scans
+# scan_N = int(len(rob_stamps)/3)
 for scan_i in range(scan_N):
     print("Scan:",scan_i)
 
@@ -64,20 +85,28 @@ for scan_i in range(scan_N):
     else:
         robt_T = robot_scan.fwd(scan_js_exe[scan_i][:6],world=True) # T_world^r2tool
         T_origin = turn_table.fwd(scan_js_exe[scan_i][6:],world=True).inv() # T_tabletool^world
-        # T_origin = turn_table.fwd(np.radians([-30,0]),world=True).inv()
+        # T_origin = turn_table.fwd(np.radians([-15,180]),world=True).inv()
     T_rob_positioner_top = T_origin*robt_T
 
     scan_points=deepcopy(mti_recording[scan_i])
-    for i in range(len(scan_points[0])):
-        print(scan_points[:,i])
-    exit()
+    scan_points=raw_data_filtering(scan_points)
+    # if scan_i==100:
+    #     for i in range(len(scan_points[0])):
+    #         print(scan_points[:,i])
+
     scan_points = np.insert(scan_points,1,np.zeros(len(scan_points[0])),axis=0)
+    scan_points[0] = -1*scan_points[0]
     scan_points = scan_points.T
     ## get the points closed to origin
     scan_points = np.transpose(np.matmul(T_rob_positioner_top.R,np.transpose(scan_points)))+T_rob_positioner_top.p
     # use legacy
     pcd = o3d.geometry.PointCloud()
     pcd.points=o3d.utility.Vector3dVector(scan_points)
+
+    # if scan_i==100:
+    #     print(np.degrees(scan_js_exe[scan_i]))
+    #     visualize_pcd([pcd])
+    #     exit()
     
     ## voxel down sample
     pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
