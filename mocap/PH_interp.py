1eb4b17debd57366b9f3be44c4c45d3b2456b397
Chen-Lung Eric Lu <eric565648@gmail.com>
2023-06-15 16:32:40 -0400

    add rbf, cubic and orientation

--- mocap/PH_interp.py
+++ mocap/PH_interp.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import LinearNDInterpolator,CloughTocher2DInterpolator,RBFInterpolator
 from copy import deepcopy
 
 class PH_Param(object):
@@ -19,21 +19,26 @@ class PH_Param(object):
         train_q=np.array(train_q)
         self.train_q=train_q
         self.method=method
+        self.data=data
 
         if method=='nearest':
-            self.data=data
             self.predict_func=self._predict_nearest
-        elif method=='linear_interp':
-            self.data=data
-            self._fit_linear_interp(data)
-            self.predict_func=self._predict_linear_interp
+        elif method=='linear':
+            self._fit_interp(LinearNDInterpolator)
+            self.predict_func=self._predict_interp
+        elif method=='cubic':
+            self._fit_interp(CloughTocher2DInterpolator)
+            self.predict_func=self._predict_interp
+        elif method=='RBF':
+            self._fit_interp(RBFInterpolator)
+            self.predict_func=self._predict_interp
         else:
             print("Choose a method")
             self.train_q=None
             self.method=None
             return False
 
-    def _fit_linear_interp(self,data):
+    def _fit_interp(self,interp_func):
         
         value_P=[]
         value_H=[]
@@ -57,22 +62,11 @@ class PH_Param(object):
         fit_P=[]
         fit_H=[]
         for val_p in value_P:
-            fit_P.append(LinearNDInterpolator(self.train_q, val_p))
+            fit_P.append(interp_func(self.train_q, val_p))
         for val_h in value_H:
-            fit_H.append(LinearNDInterpolator(self.train_q, val_h))
+            fit_H.append(interp_func(self.train_q, val_h))
         self.fit_P=fit_P
         self.fit_H=fit_H
-
-        # X = np.linspace(min(self.train_q[:,0]), max(self.train_q[:,0]),1000)
-        # Y = np.linspace(min(self.train_q[:,1]), max(self.train_q[:,1]),1000)
-        # X, Y = np.meshgrid(X, Y)
-        # Z=fit_P[3](X,Y)
-        # plt.pcolormesh(X,Y, Z, shading='auto')
-        # plt.plot(self.train_q[:,0], self.train_q[:,1], "ok", label="input point")
-        # plt.legend()
-        # plt.colorbar()
-        # plt.axis("equal")
-        # plt.show()
 
     def predict(self,q2q3):
         
@@ -81,21 +75,21 @@ class PH_Param(object):
 
         return P,H
 
-    def _predict_linear_interp(self,q2q3):
+    def _predict_interp(self,q2q3):
 
         opt_P,opt_H = deepcopy(self._predict_nearest(q2q3))
         # print("P",opt_P.T)
         # print("H",opt_H.T)
-        if np.isnan(self.fit_P[0](q2q3[0],q2q3[1])):
+        if np.isnan(self.fit_P[0]([[q2q3[0],q2q3[1]]])):
             # if outside training data, use nearest neighbor
             pass
         else:
             for i in range(len(opt_P)):
                 for j in range(len(opt_P[i])):
-                    opt_P[i][j]=self.fit_P[i*len(opt_P[i])+j](q2q3[0],q2q3[1])
+                    opt_P[i][j]=self.fit_P[i*len(opt_P[i])+j]([[q2q3[0],q2q3[1]]])
             for i in range(len(opt_H)):
                 for j in range(len(opt_H[i])):
-                    opt_H[i][j]=self.fit_H[i*len(opt_H[i])+j](q2q3[0],q2q3[1])
+                    opt_H[i][j]=self.fit_H[i*len(opt_H[i])+j]([[q2q3[0],q2q3[1]]])
             for j in range(len(opt_H[0])):
                 opt_H[:,j]=opt_H[:,j]/np.linalg.norm(opt_H[:,j])
             # print("P",opt_P.T)
