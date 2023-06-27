8f9b814b8fd2667466d3c8a57dfcf09aa6be09de
Eric Lu <eric565648@gmail.com>
2023-06-27 15:44:01 -0400

    new scanner test

---
+++ home.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('toolbox/')
+from robot_def import *
+from dx200_motion_program_exec_client import *
+
+config_dir='config/'
+robot_weld=robot_obj('MA2010_A0',def_path=config_dir+'MA2010_A0_robot_default_config.yml',d=15,tool_file_path=config_dir+'torch.csv',\
+	pulse2deg_file_path=config_dir+'MA2010_A0_pulse2deg_real.csv',\
+    base_marker_config_file=config_dir+'MA2010_marker_config.yaml',tool_marker_config_file=config_dir+'weldgun_marker_config.yaml')
+robot_scan=robot_obj('MA1440_A0',def_path=config_dir+'MA1440_A0_robot_default_config.yml',tool_file_path=config_dir+'mti.csv',\
+	base_transformation_file=config_dir+'MA1440_pose.csv',pulse2deg_file_path=config_dir+'MA1440_A0_pulse2deg_real.csv',\
+    base_marker_config_file=config_dir+'MA1440_marker_config.yaml')
+positioner=positioner_obj('D500B',def_path=config_dir+'D500B_robot_default_config.yml',tool_file_path=config_dir+'positioner_tcp.csv',\
+    base_transformation_file=config_dir+'D500B_pose.csv',pulse2deg_file_path=config_dir+'D500B_pulse2deg_real.csv',\
+    base_marker_config_file=config_dir+'D500B_marker_config.yaml',tool_marker_config_file=config_dir+'positioner_tcp_marker_config.yaml')
+
+
+q1=np.array([0,0,0,0,0,0])
+q2=np.array([90,0,0,0,0,0])
+q3=[-15,180]
+
+robot_client=MotionProgramExecClient()
+mp = MotionProgram(ROBOT_CHOICE='RB1', pulse2deg=robot_weld.pulse2deg)
+mp.MoveJ(q1,2,0)
+robot_client.execute_motion_program(mp)
+
+mp = MotionProgram(ROBOT_CHOICE='RB2', pulse2deg=robot_scan.pulse2deg)
+mp.MoveJ(q2,2,0)
+robot_client.execute_motion_program(mp)
+
+mp = MotionProgram(ROBOT_CHOICE='ST1', pulse2deg=positioner.pulse2deg)
+mp.MoveJ(q3,2,0)
+robot_client.execute_motion_program(mp)
