5afe8722cbf51827488ab7635ed7b125a1691645
hehonglu123@126.com <hehonglu123@126.com>
2023-07-07 15:28:28 -0400

    welding start/end adjustment

--- motoplus_rr/streaming_welding.py
+++ motoplus_rr/streaming_welding.py
@@ -7,12 +7,10 @@ from lambda_calc import *
 import matplotlib.pyplot as plt
 from StreamingSend import *
 
-# Adjust the connection URL to the driver
+####Adjust the connection URL to the driver
 fronius_client = RRN.ConnectService('rr+tcp://192.168.55.10:60823?service=welder')
-
-# Set the job number to use for this weld
-# fronius_client.job_number = 200
-# fronius_client.prepare_welder()
+fronius_client.job_number = 220
+fronius_client.prepare_welder()
 
 ###MOTOPLUS RR CONNECTION
 
@@ -38,8 +36,8 @@ robot=robot_obj('MA2010_A0',def_path='../config/MA2010_A0_robot_default_config.y
 R=np.array([[-0.7071, 0.7071, -0.    ],
             [ 0.7071, 0.7071,  0.    ],
             [0.,      0.,     -1.    ]])
-p_start=np.array([1630,-860,-260])
-p_end=np.array([1630,-760,-260])
+p_start=np.array([1660,-860,-260])
+p_end=np.array([1660,-760,-260])
 q_seed=np.radians([-35.4291,56.6333,40.5194,4.5177,-52.2505,-11.6546])
 base_layer_height=2
 layer_height=1.0
@@ -52,12 +50,12 @@ for i in range(0,1):
         p1=p_end+np.array([0,0,i*base_layer_height])
         p2=p_start+np.array([0,0,i*base_layer_height])
 
-    v1=20
+    v1=10
     v2=10
     p_mid1=p1+5*(p2-p1)/np.linalg.norm(p2-p1)
-    num_points1=np.ceil(125*np.linalg.norm(p1-p_mid1)/v1)
-    num_points2=np.ceil(125*np.linalg.norm(p2-p_mid1)/v2)
-    q_all=np.zeros((int(num_points1+num_points2),6))
+    num_points1=int(np.ceil(125*np.linalg.norm(p1-p_mid1)/v1))
+    num_points2=int(np.ceil(125*np.linalg.norm(p2-p_mid1)/v2))
+    q_all=[]
     for j in range(num_points1):
         q_all.append(robot.inv(p1*(num_points1-j)/num_points1+p_mid1*j/num_points1,R,q_seed)[0])
     for j in range(num_points2):
@@ -70,40 +68,36 @@ rob1_pos=robot_state.joint_position[:6]
 init_joint_pos=robot_state.joint_position
 
 ###JOG TO starting pose first
-SS.jog2q(np.hsatck((q_all[0],[np.pi/2,0,0,0,0,0,np.radians(15),-np.pi])))
+SS.jog2q(np.hstack((q_all[0],[np.pi/2,0,0,0,0,0,np.radians(-15),np.pi])))
 
 
 # ###start welding
 joint_recording=[]
 timestamp_recording=[]
 
-# fronius_client.start_weld()
+fronius_client.start_weld()
 
-curve_js_all=np.hstack((q_all,0.5*np.pi*np.ones((len(q_all),1)),np.zeros((len(q_all),5)),np.radians(15)*np.ones((len(q_all),1)),-np.pi*np.ones((len(q_all),1))))
+curve_js_all=np.hstack((q_all,0.5*np.pi*np.ones((len(q_all),1)),np.zeros((len(q_all),5)),np.radians(-15)*np.ones((len(q_all),1)),np.pi*np.ones((len(q_all),1))))
 timestamp_recording,joint_recording=SS.traj_streaming(curve_js_all[:int(len(curve_js_all)/2)])
-# fronius_client.job_number = 220
+fronius_client.job_number = 200
 timestamp_recording,joint_recording=SS.traj_streaming(curve_js_all[int(len(curve_js_all)/2):])
 
 
-# fronius_client.stop_weld()
-# fronius_client.release_welder()
+fronius_client.stop_weld()
+fronius_client.release_welder()
 
 
 
 
-# curve_exe=robot.fwd(np.array(joint_recording)).p_all
+# curve_exe=robot.fwd(np.array(joint_recording[:,:6])).p_all
 # timestamp_recording=np.array(timestamp_recording)
 # joint_recording=np.array(joint_recording)
-# timestamp_cmd=np.array(timestamp_cmd)
 
-# timestamp_cmd-=timestamp_cmd[0]
 # timestamp_recording-=timestamp_recording[0]
 # lam=calc_lam_cs(curve_exe)
 # speed=np.gradient(lam)/np.gradient(timestamp_recording)
-# speed_desired=np.gradient(calc_lam_js(q_all,robot))/np.gradient(timestamp_cmd)
 
 # plt.plot(lam,speed,label='v_exe')
-# plt.plot(lam,speed_desired,label='v_cmd')
 # plt.legend()
 # plt.show()
 
