c9cd0ea2e81f98436fbe71303ea903b17bd877f0
hehonglu123@126.com <hehonglu123@126.com>
2023-07-26 11:23:16 -0400

    3 robot wall welding

--- weld/weld_wall_tri.py
+++ weld/weld_wall_tri.py
@@ -5,16 +5,30 @@ from lambda_calc import *
 from multi_robot import *
 from dx200_motion_program_exec_client import *
 from WeldSend import *
-sys.path.append('../scan/scan_tools/')
-sys.path.append('../scan/scan_plan/')
-sys.path.append('../scan/scan_process/')
-from scan_utils import *
-from scanPathGen import *
-from scanProcess import *
-from weldRRSensor import *
 from RobotRaconteur.Client import *
-from copy import deepcopy
-from pathlib import Path
+
+timestamp=[]
+voltage=[]
+current=[]
+feedrate=[]
+energy=[]
+
+def wire_cb(sub, value, ts):
+	global timestamp, voltage, current, feedrate, energy
+
+	timestamp.append(value.ts['microseconds'][0])
+	voltage.append(value.welding_voltage)
+	current.append(value.welding_current)
+	feedrate.append(value.wire_speed)
+	energy.append(value.welding_energy)
+
+
+logging=False
+if logging:
+	sub=RRN.SubscribeService('rr+tcp://192.168.55.10:60823?service=welder')
+	obj = sub.GetDefaultClientWait(3)      #connect, timeout=30s
+	welder_state_sub=sub.SubscribeWire("welder_state")
+	welder_state_sub.WireValueChanged += wire_cb
 
 
 dataset='wall/'
@@ -30,50 +44,20 @@ layer_width_num=int(4/slicing_meta['line_resolution'])
 
 
 robot=robot_obj('MA2010_A0',def_path='../config/MA2010_A0_robot_default_config.yml',tool_file_path='../config/torch.csv',\
-	pulse2deg_file_path='../config/MA2010_A0_pulse2deg_real.csv',d=15,\
-    base_marker_config_file='../config/MA2010_marker_config.yaml',tool_marker_config_file='../config/weldgun_marker_config.yaml')
+	pulse2deg_file_path='../config/MA2010_A0_pulse2deg_real.csv',d=15)
 robot2=robot_obj('MA1440_A0',def_path='../config/MA1440_A0_robot_default_config.yml',tool_file_path='../config/flir.csv',\
-	pulse2deg_file_path='../config/MA1440_A0_pulse2deg_real.csv',base_transformation_file='../config/MA1440_pose.csv',\
-    base_marker_config_file='../config/MA1440_marker_config.yaml')
+	pulse2deg_file_path='../config/MA1440_A0_pulse2deg_real.csv',base_transformation_file='../config/MA1440_pose.csv')
 positioner=positioner_obj('D500B',def_path='../config/D500B_robot_default_config.yml',tool_file_path='../config/positioner_tcp.csv',\
-	pulse2deg_file_path='../config/D500B_pulse2deg_real.csv',base_transformation_file='../config/D500B_pose_mocap.csv',\
-    base_marker_config_file='../config/D500B_marker_config.yaml',tool_marker_config_file='../config/positioner_tcp_marker_config.yaml')
-
-# Table_home_T = positioner.fwd(np.radians([-15,180]))
-# T_S1TCP_R1Base = np.linalg.inv(np.matmul(positioner.base_H,H_from_RT(Table_home_T.R,Table_home_T.p)))
-# T_R1Base_S1TCP = np.linalg.inv(T_S1TCP_R1Base)
-
-#### change base H to calibrated ones ####
-robot2.base_H = H_from_RT(robot2.T_base_basemarker.R,robot2.T_base_basemarker.p)
-positioner.base_H = H_from_RT(positioner.T_base_basemarker.R,positioner.T_base_basemarker.p)
-T_to_base = Transform(np.eye(3),[0,0,-380])
-positioner.base_H = np.matmul(positioner.base_H,H_from_RT(T_to_base.R,T_to_base.p))
+	pulse2deg_file_path='../config/D500B_pulse2deg_real.csv',base_transformation_file='../config/D500B_pose_mocap.csv')
 
 client=MotionProgramExecClient()
 ws=WeldSend(client)
 
-## rr drivers and all other drivers
-robot_client=MotionProgramExecClient()
-# weld state logging
-weld_ser = RRN.SubscribeService('rr+tcp://192.168.55.10:60823?service=welder')
-cam_ser=RRN.ConnectService('rr+tcp://192.168.55.10:60827/?service=camera')
-mic_ser = RRN.ConnectService('rr+tcp://192.168.55.20:60828?service=microphone')
-## RR sensor objects
-rr_sensors = WeldRRSensor(weld_service=weld_ser,cam_service=cam_ser,microphone_service=mic_ser)
-
-# MTI connect to RR
-mti_client = RRN.ConnectService("rr+tcp://192.168.55.10:60830/?service=MTI2D")
-mti_client.setExposureTime("25")
-
 ###########################################layer welding############################################
-num_layer_start=int(10*layer_height_num)	###modify layer num here
-num_layer_end=int(11*layer_height_num)
-# q_prev=client.getJointAnglesDB(positioner.pulse2deg)
-q_prev=np.array([-3.791544713877046391e-01,7.156749523014762637e-01,2.756772964158371586e-01,2.106493295914119712e-01,-7.865937103692784982e-01,-5.293956242391706368e-01])	###for motosim tests only
-
-## for scanning ##
-h_largest=0
-##################
+num_layer_start=int(4*layer_height_num)	###modify layer num here
+num_layer_end=int(5*layer_height_num)
+q_prev=client.getJointAnglesMH(robot.pulse2deg)
+# q_prev=np.array([-3.791544713877046391e-01,7.156749523014762637e-01,2.756772964158371586e-01,2.106493295914119712e-01,-7.865937103692784982e-01,-5.293956242391706368e-01])	###for motosim tests only
 
 if num_layer_start<=1*layer_height_num:
 	num_sections=len(glob.glob(data_dir+'curve_sliced_relative/slice0_*.csv'))
@@ -103,7 +87,7 @@ for layer in range(num_layer_start,num_layer_end,layer_height_num):
 		num_points_layer=max(2,int(lam_relative[-1]/waypoint_distance))
 
 		###find which end to start depending on how close to joint limit
-		if positioner.upper_limit[1]-q_prev[1]>q_prev[1]-positioner.lower_limit[1]:
+		if np.linalg.norm(q_prev-rob1_js[0])<np.linalg.norm(q_prev-rob1_js[-1]):
 			breakpoints=np.linspace(0,len(rob1_js)-1,num=num_points_layer).astype(int)
 		else:
 			breakpoints=np.linspace(len(rob1_js)-1,0,num=num_points_layer).astype(int)
@@ -135,127 +119,7 @@ for layer in range(num_layer_start,num_layer_end,layer_height_num):
 
 		q_prev=positioner_js[breakpoints[-1]]
 	
+
 		###robot1=robot2 speed tests
-		rr_sensors.start_all_sensors()
-		timestamp_robot,joint_recording,job_line,_=ws.weld_segment_tri(primitives,robot,positioner,robot2,q1_all,positioner_all,q2_all,v1_all,v1_all,cond_all=[200],arc=False)
-		rr_sensors.stop_all_sensors()
-		rr_sensors.save_all_sensors('')
+		timestamp_robot,joint_recording,job_line,_=ws.weld_segment_tri(primitives,robot,positioner,robot2,q1_all,positioner_all,q2_all,v1_all,v1_all,cond_all=[200],arc=True)
 		np.savetxt('joint_recording.csv',np.hstack((timestamp_robot.reshape(-1, 1),job_line.reshape(-1, 1),joint_recording)),delimiter=',')
-  
-	######## scanning ##########
-	for x in range(0,num_sections,layer_width_num):
-		curve_sliced_relative=np.loadtxt(data_dir+'curve_sliced_relative/slice'+str(layer)+'_'+str(x)+'.csv',delimiter=',')
-
-		scan_speed=10 # scanning speed (mm/sec)
-		scan_stand_off_d = 95 ## mm
-		Rz_angle = np.radians(0) # point direction w.r.t welds
-		Ry_angle = np.radians(0) # rotate in y a bit
-		bounds_theta = np.radians(1) ## circular motion at start and end
-		all_scan_angle = np.radians([0]) ## scan angle
-		q_init_table=np.radians([-15,200]) ## init table
-		save_output_points = True
-		### scanning path module
-		spg = ScanPathGen(robot2,positioner,scan_stand_off_d,Rz_angle,Ry_angle,bounds_theta)
-		mti_Rpath = np.array([[ -1.,0.,0.],   
-					[ 0.,1.,0.],
-					[0.,0.,-1.]])
-		scan_p,scan_R,q_out1,q_out2=spg.gen_scan_path([curve_sliced_relative],[0],all_scan_angle,\
-                            solve_js_method=0,q_init_table=q_init_table,R_path=mti_Rpath,scan_path_dir=None)
-		q_bp1,q_bp2,s1_all,s2_all=spg.gen_motion_program(q_out1,q_out2,scan_p,scan_speed,init_sync_move=0)
-
-		to_start_speed=7
-		mp = MotionProgram(ROBOT_CHOICE='RB2',ROBOT_CHOICE2='ST1',pulse2deg=robot2.pulse2deg,pulse2deg_2=positioner.pulse2deg)
-		target2=['MOVJ',np.degrees(q_bp2[0][0]),to_start_speed]
-		mp.MoveJ(np.degrees(q_bp1[0][0]), to_start_speed, 0, target2=target2)
-		robot_client.execute_motion_program(mp)
-
-		scan_motion_scan_st = time.time()
-
-		## motion start
-		mp = MotionProgram(ROBOT_CHOICE='RB2',ROBOT_CHOICE2='ST1',pulse2deg=robot2.pulse2deg,pulse2deg_2=positioner.pulse2deg)
-		# calibration motion
-		target2=['MOVJ',np.degrees(q_bp2[1][0]),s2_all[0]]
-		mp.MoveL(np.degrees(q_bp1[1][0]), scan_speed, 0, target2=target2)
-		# routine motion
-		for path_i in range(2,len(q_bp1)-1):
-			target2=['MOVJ',np.degrees(q_bp2[path_i][0]),s2_all[path_i]]
-			mp.MoveL(np.degrees(q_bp1[path_i][0]), s1_all[path_i], target2=target2)
-		target2=['MOVJ',np.degrees(q_bp2[-1][0]),s2_all[-1]]
-		mp.MoveL(np.degrees(q_bp1[-1][0]), s1_all[-1], 0, target2=target2)
-
-		robot_client.execute_motion_program_nonblocking(mp)
-		###streaming
-		robot_client.StartStreaming()
-		start_time=time.time()
-		state_flag=0
-		joint_recording=[]
-		robot_stamps=[]
-		mti_recording=[]
-		r_pulse2deg = np.append(robot2.pulse2deg,positioner.pulse2deg)
-		while True:
-			if state_flag & 0x08 == 0 and time.time()-start_time>1.:
-				break
-			res, data = robot_client.receive_from_robot(0.01)
-			if res:
-				joint_angle=np.radians(np.divide(np.array(data[26:34]),r_pulse2deg))
-				state_flag=data[16]
-				joint_recording.append(joint_angle)
-				timestamp=data[0]+data[1]*1e-9
-				robot_stamps.append(timestamp)
-				###MTI scans YZ point from tool frame
-				mti_recording.append(deepcopy(np.array([mti_client.lineProfile.X_data,mti_client.lineProfile.Z_data])))
-		robot_client.servoMH(False)
-		mti_recording=np.array(mti_recording)
-		q_out_exe=joint_recording
-
-		# move robot to home
-		q2=np.zeros(6)
-		q2[0]=90
-		q3=[-15,180]
-		## move to home
-		to_home_speed=7
-		mp = MotionProgram(ROBOT_CHOICE='RB2',ROBOT_CHOICE2='ST1',pulse2deg=robot2.pulse2deg,pulse2deg_2=positioner.pulse2deg)
-		target2=['MOVJ',q3,to_home_speed]
-		mp.MoveJ(q2, to_home_speed, 0, target2=target2)
-		robot_client.execute_motion_program(mp)
-		#####################
-
-		print("Total exe len:",len(q_out_exe))
-		out_scan_dir = layer_data_dir+'scans/'
-		## save traj
-		Path(out_scan_dir).mkdir(exist_ok=True)
-		# save poses
-		np.savetxt(out_scan_dir + 'scan_js_exe.csv',q_out_exe,delimiter=',')
-		np.savetxt(out_scan_dir + 'scan_robot_stamps.csv',robot_stamps,delimiter=',')
-		with open(out_scan_dir + 'mti_scans.pickle', 'wb') as file:
-			pickle.dump(mti_recording, file)
-		print('Total scans:',len(mti_recording))
-  
-		curve_x_start = deepcopy(curve_sliced_relative[0][0])
-		curve_x_end = deepcopy(curve_sliced_relative[-1][0])
-		# Transz0_H=np.array([[ 9.99974559e-01, -7.29664987e-06, -7.13309345e-03, -1.06461758e-02],
-		#                     [-7.29664987e-06,  9.99997907e-01, -2.04583032e-03, -3.05341146e-03],
-		#                     [ 7.13309345e-03,  2.04583032e-03,  9.99972466e-01,  1.49246365e+00],
-		#                     [ 0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000000e+00]])
-		z_height_start=h_largest-3
-		crop_extend=10
-		crop_min=(curve_x_end-crop_extend,-30,-10)
-		crop_max=(curve_x_start+crop_extend,30,z_height_start+30)
-		crop_h_min=(curve_x_end-crop_extend,-20,-10)
-		crop_h_max=(curve_x_start+crop_extend,20,z_height_start+30)
-		scan_process = ScanProcess(robot2,positioner)
-		pcd = scan_process.pcd_register_mti(mti_recording,q_out_exe,robot_stamps,static_positioner_q=q_init_table)
-		pcd = scan_process.pcd_noise_remove(pcd,nb_neighbors=40,std_ratio=1.5,\
-											min_bound=crop_min,max_bound=crop_max,cluster_based_outlier_remove=True,cluster_neighbor=1,min_points=100)
-		profile_height,Transz0_H = scan_process.pcd2height(deepcopy(pcd),z_height_start,bbox_min=crop_h_min,bbox_max=crop_h_max,Transz0_H=Transz0_H)
-		print("Transz0_H:",Transz0_H)
-
-		save_output_points=True
-		if save_output_points:
-			o3d.io.write_point_cloud(out_scan_dir+'processed_pcd.pcd',pcd)
-			np.save(out_scan_dir+'height_profile.npy',profile_height)
-		# visualize_pcd([pcd])
-		plt.scatter(profile_height[:,0],profile_height[:,1])
-		plt.show()
-
-		h_largest=np.max(profile_height[:,1])
