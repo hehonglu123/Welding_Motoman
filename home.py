a8d0e8b8077944167ba6e2e4c1889f759a3b3e19
Eric Lu <eric565648@gmail.com>
2023-08-09 16:32:18 -0400

    R1 tool calib data

--- home.py
+++ home.py
@@ -16,22 +16,22 @@ positioner=positioner_obj('D500B',def_path=config_dir+'D500B_robot_default_confi
 
 
 q1=np.array([0,0,0,0,0,0])
-q2=np.array([0,0,0,0,0,0])
+q2=np.array([90,0,0,0,0,0])
 # q2=np.array([0,37,-9,0,0,0])
 # q2_1=np.array([0,36,-8,0,0,0])
 q3=[-15,180]
 
 robot_client=MotionProgramExecClient()
 
-# mp = MotionProgram(ROBOT_CHOICE='RB1', pulse2deg=robot_weld.pulse2deg)
-# mp.MoveJ(q1,5,0)
-# robot_stamps,curve_exe, job_line,job_step = robot_client.execute_motion_program(mp)
-# print("Test read joints:",np.degrees(curve_exe[-1]))
+mp = MotionProgram(ROBOT_CHOICE='RB1', pulse2deg=robot_weld.pulse2deg)
+mp.MoveJ(q1,5,0)
+robot_stamps,curve_exe, job_line,job_step = robot_client.execute_motion_program(mp)
+print("Test read joints:",np.degrees(curve_exe[-1]))
 
 mp = MotionProgram(ROBOT_CHOICE='RB2', pulse2deg=robot_scan.pulse2deg)
 mp.MoveJ(q2,5,0)
 robot_client.execute_motion_program(mp)
 
-# mp = MotionProgram(ROBOT_CHOICE='ST1', pulse2deg=positioner.pulse2deg)
-# mp.MoveJ(q3,5,0)
-# robot_client.execute_motion_program(mp)
+mp = MotionProgram(ROBOT_CHOICE='ST1', pulse2deg=positioner.pulse2deg)
+mp.MoveJ(q3,5,0)
+robot_client.execute_motion_program(mp)
