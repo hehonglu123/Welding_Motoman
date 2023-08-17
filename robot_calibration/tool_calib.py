b8840391c75b53bf0e25d8afaf20ca67a1813785
Chen-Lung Eric Lu <eric565648@gmail.com>
2023-08-17 11:09:41 -0400

    test out mti calibration

--- robot_calibration/tool_calib.py
+++ robot_calibration/tool_calib.py
@@ -19,6 +19,10 @@ data_dir='tool_data/'+'R2_mti_0809/'
 ph_dataset_date='0804'
 
 config_dir='../config/'
+R1_marker_dir=config_dir+'MA2010_marker_config/'
+weldgun_marker_dir=config_dir+'weldgun_marker_config/'
+R2_marker_dir=config_dir+'MA1440_marker_config/'
+mti_marker_dir=config_dir+'mti_marker_config/'
 
 robot_type = 'R2'
 
@@ -29,8 +33,8 @@ if robot_type == 'R1':
                         tool_file_path=config_dir+'torch.csv',d=15,\
                         #  tool_file_path='',d=0,\
                         pulse2deg_file_path=config_dir+'MA2010_A0_pulse2deg_real.csv',\
-                        base_marker_config_file=config_dir+'MA2010_'+ph_dataset_date+'_marker_config.yaml',\
-                        tool_marker_config_file=config_dir+'weldgun_'+ph_dataset_date+'_marker_config.yaml')
+                        base_marker_config_file=R1_marker_dir+'MA2010_'+ph_dataset_date+'_marker_config.yaml',\
+                        tool_marker_config_file=weldgun_marker_dir+'weldgun_'+ph_dataset_date+'_marker_config.yaml')
     nom_P=np.array([[0,0,0],[150,0,0],[0,0,760],\
                    [1082,0,200],[0,0,0],[0,0,0],[100,0,0]]).T
     nom_H=np.array([[0,0,1],[0,1,0],[0,-1,0],\
@@ -39,8 +43,8 @@ elif robot_type == 'R2':
     robot=robot_obj('MA1440_A0',def_path=config_dir+'MA1440_A0_robot_default_config.yml',\
                         tool_file_path=config_dir+'mti.csv',\
                         pulse2deg_file_path=config_dir+'MA1440_A0_pulse2deg_real.csv',\
-                        base_marker_config_file=config_dir+'MA1440_'+ph_dataset_date+'_marker_config.yaml',\
-                        tool_marker_config_file=config_dir+'mti_'+ph_dataset_date+'_marker_config.yaml')
+                        base_marker_config_file=R2_marker_dir+'MA1440_'+ph_dataset_date+'_marker_config.yaml',\
+                        tool_marker_config_file=mti_marker_dir+'mti_'+ph_dataset_date+'_marker_config.yaml')
     nom_P=np.array([[0,0,0],[155,0,0],[0,0,614],\
                    [640,0,200],[0,0,0],[0,0,0],[100,0,0]]).T
     nom_H=np.array([[0,0,1],[0,1,0],[0,-1,0],\
@@ -60,8 +64,8 @@ robot.robot.p_tool = np.zeros(3)
 robot_Ts=[]
 for i in range(num_js):
     q=np.loadtxt(data_dir+'pose_js_'+str(i)+'.csv',delimiter=',')
-    robot_T=robot.fwd_ph(q,ph_param)
-    # robot_T=robot.fwd(q)
+    # robot_T=robot.fwd_ph(q,ph_param)
+    robot_T=robot.fwd(q)
     robot_Ts.append(H_from_RT(robot_T.R,robot_T.p))
 
 A=[]
