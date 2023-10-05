ab16e999fe9b7d3ae2a53fa5270044b0a69f9144
hehonglu123@126.com <hehonglu123@126.com>
2023-10-05 17:07:51 -0400

    3D plot

--- FLIR/flow/pixel_value_all_3D.py
+++ FLIR/flow/pixel_value_all_3D.py
@@ -20,9 +20,12 @@ for i in range(2,10):
     v=data['weld_v'].tolist()[-1][1:-1]
     ir_ts=np.loadtxt('../../../recorded_data/316L_model_120ipm_2023_09_25_19_56_43/layer_%i/ir_stamps.csv'%(i), delimiter=',')
 
-    columns=range(88,120)
+    columns=range(88,150)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    ts_all=[]
+    pixel_all=[]
+    counts_all=[]
     for column in columns:
         #canny edge detection
         ir_normalized=normalize2cv(ir_recording[0])
@@ -34,16 +37,19 @@ for i in range(2,10):
             if ir_recording[0][index+3,column]>100:
                 break
         
-        ax.plot3D(ir_ts, [column]*len(ir_ts), ir_recording[:,index+3,column].flatten(), 'green')
+        ts_all.extend(ir_ts)
+        pixel_all.extend([column]*len(ir_ts))
+        counts_all.extend(ir_recording[:,index+3,column].flatten())
+    # ax.plot3D(ts_all, pixel_all, counts_all)
+    surf = ax.plot_trisurf(ts_all, pixel_all, counts_all, linewidth=0, antialiased=False, label='-')
 
     # plt.plot(ir_ts, ir_recording[:,index+3,88],label='v='+v+'mm/s')
 
-plt.title('Pixel Value vs Time')
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('Column/X (pixel)')
-ax.set_zlabel('Pixel Value (Counts)')
+    plt.title('Pixel Value vs Time')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Column/X (pixel)')
+    ax.set_zlabel('Pixel Value (Counts)')
 
-plt.legend()
 plt.show()
 
 
