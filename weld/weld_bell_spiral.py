1a1b67789c2c20d39bf58979d6fd98d5c431358a
hehonglu123 <hehonglu123@126.com>
2023-10-17 13:50:23 -0400

    bell316

--- weld/weld_bell_spiral.py
+++ weld/weld_bell_spiral.py
@@ -99,10 +99,30 @@ q_prev=client.getJointAnglesDB(positioner.pulse2deg)
 # vd_relative_adjustment=2
 
 
-###set up control parameters, no baselayer needed for 316L!!!!
+# ###set up control parameters, no baselayer needed for 316L!!!!
+# job_offset=400 		###200 for Aluminum ER4043, 300 for Steel Alloy ER70S-6, 400 for Stainless Steel ER316L
+# nominal_feedrate=80
+# nominal_vd_relative=3
+# nominal_wire_length=25 #pixels
+# nominal_temp_below=500
+# base_feedrate_cmd=300
+# base_vd=8
+# feedrate_cmd=nominal_feedrate
+# vd_relative=nominal_vd_relative
+# feedrate_gain=0.5
+# feedrate_min=80
+# feedrate_max=300
+# nominal_slice_increment=int(1.15/slicing_meta['line_resolution'])
+# slice_inc_gain=3.
+# vd_max=8
+# feedrate_cmd_adjustment=1
+# vd_relative_adjustment=1
+
+
+###set up control parameters (backup)
 job_offset=400 		###200 for Aluminum ER4043, 300 for Steel Alloy ER70S-6, 400 for Stainless Steel ER316L
 nominal_feedrate=80
-nominal_vd_relative=3
+nominal_vd_relative=2
 nominal_wire_length=25 #pixels
 nominal_temp_below=500
 base_feedrate_cmd=300
@@ -112,12 +132,11 @@ vd_relative=nominal_vd_relative
 feedrate_gain=0.5
 feedrate_min=80
 feedrate_max=300
-nominal_slice_increment=int(1.15/slicing_meta['line_resolution'])
+nominal_slice_increment=int(1.1/slicing_meta['line_resolution'])
 slice_inc_gain=3.
 vd_max=8
 feedrate_cmd_adjustment=1
 vd_relative_adjustment=1
-
 
 
 ######################################################BASE LAYER##########################################################################################
@@ -190,12 +209,12 @@ positioner_js_all_slices=[]
 lam_relative_all_slices=[]
 lam_relative_dense_all_slices=[]
 for i in range(0,slicing_meta['num_layers']):
-	# rob1_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_0.csv',delimiter=','))
-	# rob2_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/MA1440_js'+str(i)+'_0.csv',delimiter=','))
-	# positioner_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_0.csv',delimiter=','))
-	rob1_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_0.csv',delimiter=','),axis=0))
-	rob2_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA1440_js'+str(i)+'_0.csv',delimiter=','),axis=0))
-	positioner_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_0.csv',delimiter=','),axis=0))
+	rob1_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_0.csv',delimiter=','))
+	rob2_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/MA1440_js'+str(i)+'_0.csv',delimiter=','))
+	positioner_js_all_slices.append(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_0.csv',delimiter=','))
+	# rob1_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA2010_js'+str(i)+'_0.csv',delimiter=','),axis=0))
+	# rob2_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/MA1440_js'+str(i)+'_0.csv',delimiter=','),axis=0))
+	# positioner_js_all_slices.append(np.flip(np.loadtxt(data_dir+'curve_sliced_js/D500B_js'+str(i)+'_0.csv',delimiter=','),axis=0))
 	
 print("PRELOAD FINISHED")
 
