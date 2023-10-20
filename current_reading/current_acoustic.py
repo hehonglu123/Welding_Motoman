71ad742d6b23919c03d6aefec7d53ce9d11e078b
JInhan05013 <qhxnrjh@gmail.com>
2023-10-19 21:21:22 -0400

    data_fusion

--- current_reading/current_acoustic.py
+++ current_reading/current_acoustic.py
@@ -3,38 +3,139 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 from scipy.signal import resample
+from scipy.interpolate import interp1d
+import librosa
+import os
 
-base_path = '../data/wall_weld_test/ER4043_correction_100ipm_2023_09_27_20_53_05/layer_7/'
+def load_height_profile(base_path, layer_num):
+    path = os.path.join(base_path.replace('#', str(layer_num)), 'scans', 'height_profile.npy')
+    if os.path.exists(path):
+        return np.load(path)
+    else:
+        return None
+
+def compute_dh(base_path, layer_num):
+    current_profile = load_height_profile(base_path, layer_num)
+    if current_profile is None:
+        return None
+    
+    if layer_num == 0:
+        return current_profile
+    
+    previous_profile = load_height_profile(base_path, layer_num - 1)
+    if previous_profile is None:
+        return None
+    
+    # if layer_num % 2 == 1:  # 当前层为反向
+    #     previous_profile = previous_profile[::-1]
+    
+    # 如果两层数据长度不同，使它们具有相同长度
+    min_length = min(len(current_profile), len(previous_profile))
+    current_profile = current_profile[:min_length]
+    previous_profile = previous_profile[:min_length]
+    
+    dh = current_profile - previous_profile
+    return dh
+
+base_path = '../data/wall_weld_test/ER4043_correction_100ipm_2023_09_27_20_53_05/layer_#/'
+layer_num = 4  # 修改为你的层数
+dh = compute_dh(base_path, layer_num)
+tip_dis = (np.mean(dh) + 2.3420716473455623) - dh
+base_path = '../data/wall_weld_test/ER4043_correction_100ipm_2023_09_27_20_53_05/layer_4/'
 fs_wav, audio_data = wav.read(base_path + "mic_recording.wav")
-
+height_path = '../data/wall_weld_test/ER4043_correction_100ipm_2023_09_27_20_53_05/'
 
 df = pd.read_csv(base_path + "current.csv")
 time_stamps = df['timestamp'].values
 current_signal = df['current'].values
 
+# 读取坐标文件
+height_profile = np.load(base_path + 'scans/' + "height_profile.npy")
+print('print(height_profile.shape):',height_profile.shape)
+# 创建与电流信号相同长度的时间轴
+time_axis = np.linspace(0, len(current_signal) / fs_wav, len(current_signal))
+print('time_axis:',time_axis.shape)
 
-plt.figure(figsize=(10, 6))
+# 创建一个新的图形
+# 1. 计算电流信号的平均值
+threshold_start = 10
+threshold_end = 10
+# 2. 寻找电流信号中第一个超过阈值的点
+start_index = np.where(current_signal > threshold_start)[0][0]
 
-# 绘制声音信号
-plt.subplot(2, 1, 1)
-audio_duration = len(audio_data) / fs_wav  # 计算音频的持续时间
-plt.plot(np.linspace(0, audio_duration, len(audio_data)), audio_data)
-plt.title('Audio Signal')
+# 3. 从起始点开始，寻找电流信号中第一个低于阈值的点
+end_index = np.where(current_signal[start_index:] < threshold_end)[0][0] + start_index
+
+# 4. 使用这两个时间点截取声音信号
+audio_start_index = int(start_index * len(audio_data) / len(current_signal))
+audio_end_index = int(end_index * len(audio_data) / len(current_signal))
+audio_clipped = audio_data[audio_start_index:audio_end_index]
+
+# 对height_profile进行线性插值，使其与time_axis长度相同
+height_interpolated = interp1d(np.linspace(0, 1, len(dh)), dh[:, 1])(np.linspace(0, 1, end_index - start_index))
+tip_interpolated = interp1d(np.linspace(0, 1, len(tip_dis)), tip_dis[:, 1])(np.linspace(0, 1, end_index - start_index))
+# 先解析base_path中的layer_#部分
+layer_number = int(base_path.split('layer_')[-1].split('/')[0])
+
+# 判断layer_number是奇数还是偶数
+if layer_number % 2 == 0:  # 偶数
+    direction = "left-to-right"
+else:  # 奇数
+    direction = "right-to-left"
+
+# # 如果是从右往左，就反转height_profile
+if direction == "left-to-right":
+    height_interpolated = height_interpolated[::-1]
+    tip_interpolated = tip_interpolated[::-1]
+
+# 以下是展示截取后的数据
+plt.figure(figsize=(10, 8))
+
+# 确定电流信号的时间轴的起始和结束时间
+current_time_start = time_stamps[start_index]
+current_time_end = time_stamps[end_index]
+
+# 计算截取的声音信号的时间轴
+audio_time_stamps = np.linspace(current_time_start, current_time_end, len(audio_clipped))
+
+# 绘制截取后的声音信号
+plt.subplot(3, 1, 1)
+plt.plot(audio_time_stamps, audio_clipped)
+plt.title('Acoustic Signal')
 plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
+plt.ylabel('Amplitude (Pa)')
 
-# 绘制current信号
-plt.subplot(2, 1, 2)
-plt.plot(time_stamps, current_signal)
+# 绘制截取后的电流信号
+plt.subplot(3, 1, 2)
+plt.plot(time_stamps[start_index:end_index], current_signal[start_index:end_index])
 plt.title('Current Signal')
 plt.xlabel('Time (s)')
-plt.ylabel('Current Value')
+plt.ylabel('Current (A)')
 
+# 绘制截取后的坐标信号
+plt.subplot(3, 1, 3)
+plt.plot(time_stamps[start_index:end_index], height_interpolated)
+if direction == "left-to-right":
+    plt.title('Layer by Layer Height Profile')
+else:
+    plt.title('Layer by Layer Height Profile')
+plt.xlabel('Time (s)')
+plt.ylabel('Height (mm)')
+
+# 绘制截取后的坐标信号
+# plt.subplot(4, 1, 4)
+# plt.plot(time_stamps[start_index:end_index], tip_interpolated)
+# if direction == "left-to-right":
+#     plt.title('Tip Distance Profile (Left to Right)')
+# else:
+#     plt.title('Tip Distance Profile (Right to Left)')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Tip Distance (mm)')
 plt.tight_layout()
 plt.show()
-
+###################################################################################################################
 t_start = 4.3  # 起始时间，你可以根据需要修改
-t_end = t_start + 0.2  # 结束时间
+t_end = t_start + 0.3  # 结束时间
 
 # 从音频信号中截取
 audio_start_idx = int(t_start * fs_wav)
@@ -61,6 +162,53 @@ plt.plot(time_subset, current_subset)
 plt.title('Current Signal Subset')
 plt.xlabel('Time (s)')
 plt.ylabel('Current Value')
+
+plt.tight_layout()
+plt.show()
+########################################################################################################################
+# 1. Calculate energy of audio signal with respect to time
+
+frame_length = int(fs_wav * 0.02)  # 20 ms frames
+hop_length = int(fs_wav * 0.01)  # 10 ms overlap
+audio_clipped = audio_clipped.astype(np.float32) / np.iinfo(np.int16).max
+
+# Calculate energy for each frame
+energy = np.array([np.sum(np.abs(audio_clipped[i:i+frame_length]**2)) for i in range(0, len(audio_clipped), hop_length)])
+
+# 2. Calculate RMSE with respect to time
+rmse_time = librosa.feature.rms(y=audio_clipped, frame_length=frame_length, hop_length=hop_length)
+
+# 3. Calculate RMSE with respect to frequency
+D = librosa.stft(audio_clipped, n_fft=2048, hop_length=hop_length, win_length=frame_length)
+
+spectral_rms = np.sqrt(np.mean(np.abs(D)**2, axis=1))
+
+# Plotting
+plt.figure(figsize=(15, 10))
+
+# Energy vs Time
+plt.subplot(3, 1, 1)
+plt.plot(np.arange(len(energy)) * hop_length / fs_wav, energy)
+plt.title('Energy vs Time')
+plt.xlabel('Time (s)')
+plt.ylabel('Energy')
+
+# RMSE vs Time
+plt.subplot(3, 1, 2)
+times = np.arange(rmse_time.shape[1]) * hop_length / fs_wav
+plt.plot(times, rmse_time[0])
+plt.title('RMSE vs Time')
+plt.xlabel('Time (s)')
+plt.ylabel('RMSE')
+
+# RMSE vs Frequency
+plt.subplot(3, 1, 3)
+freqs = librosa.fft_frequencies(sr=fs_wav, n_fft=2048)
+plt.plot(freqs, spectral_rms)
+plt.title('RMSE vs Frequency')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('RMSE')
+plt.xlim([0, fs_wav / 2])  # display only up to Nyquist frequency
 
 plt.tight_layout()
 plt.show()
--- microphone/PCA_MFCC.py
+++ microphone/PCA_MFCC.py
@@ -12,6 +12,7 @@ import os
 import re
 from matplotlib.ticker import MaxNLocator
 from matplotlib.animation import FuncAnimation
+import time
 
 def moving_average(data_list, window_size):
     weights = np.ones(window_size) / window_size
@@ -28,18 +29,15 @@ mean_co2 = []
 mean_value_co1 = []
 mean_value_co2 = []
 window_length = []
-base_path = '../data/wall_weld_test/ER4043_correction_100ipm_2023_09_27_20_53_05/'
+base_path = '../data/wall_weld_test/316L_model_130ipm_2023_10_16_22_53_13/'
 
 if os.path.exists(base_path):
     # 获取指定路径下的所有子目录
     subdirs = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
-
     # 使用正则表达式匹配 layer_n 模式的子目录
     layer_dirs = [d for d in subdirs if re.match(r'layer_\d+', d)]
-
     for layer_dir in sorted(layer_dirs, key=lambda x: int(x.split('_')[-1])):
         layer_path = os.path.join(base_path, layer_dir + '/',)
-
         # Construct the path to the mic_recording.wav file
         mic_recording_path = os.path.join(layer_path, "mic_recording_cut.wav")
         
@@ -56,7 +54,6 @@ if os.path.exists(base_path):
         while n < 1:
         # 加载音频文件
             y, sr = librosa.load(layer_path + 'mic_recording_cut.wav', sr=None)
-
             # 计算MFCC
             mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)  # 获取13个MFCC系数
             print("MFCCs shape:", mfccs.shape)  # 此处得到的形状通常为(13, 时间帧数)
@@ -73,8 +70,6 @@ if os.path.exists(base_path):
             plt.tight_layout()
             # plt.show()
             plt.close()
-
-            
             for i in range(2):
                 plt.plot(mfccs[i], label=f'MFCC co {i+1}')
                 plt.axhline(y=np.mean(mfccs[i]), color='r', linestyle='-', label=f'MFCC co_mean {i+1}')
@@ -85,57 +80,6 @@ if os.path.exists(base_path):
             plt.legend()
             plt.show()
             plt.close()
-            n += 1 
-            continue
-            # 设置y轴的范围为两组mfccs数据的最小值和最大值
-            fig, ax = plt.subplots()
-            ax.set_ylim(min(np.min(mfccs[0]), np.min(mfccs[1])) - 1, max(np.max(mfccs[0]), np.max(mfccs[1])) + 1)
-
-            # 创建两个空的数据集来收集x和y的数据
-            xdata1, ydata1, xdata2, ydata2 = [], [], [], []
-            ln2, = ax.plot([], [], 'b-', animated=True, label='MFCC Coefficient 2')
-            ln1, = ax.plot([], [], 'g-', animated=True, label='MFCC Coefficient 1')
-
-            def init():
-                ax.set_xlim(0, max(len(mfccs[0]), len(mfccs[1])))
-                ax.set_ylabel('MFCC Coefficients')
-                ax.set_xlabel('number of frames')
-                ax.set_title(f'MFCC 1st and 2nd coefficients of {layer_dir}')
-                ax.legend(loc = 'lower right')
-                return ln1, ln2
-
-            def update(frame):
-                if frame < len(mfccs[0]):
-                    xdata1.append(frame)
-                    ydata1.append(mfccs[0][frame])
-                    ln1.set_data(xdata1, ydata1)
-                if frame < len(mfccs[1]):
-                    xdata2.append(frame)
-                    ydata2.append(mfccs[1][frame])
-                    ln2.set_data(xdata2, ydata2)
-                return ln1, ln2
-
-            total_time_milliseconds = 5000
-            total_frames = max(len(mfccs[0]), len(mfccs[1]))
-
-            interval_time = total_time_milliseconds / total_frames
-            
-            ani = FuncAnimation(fig, update, frames=range(total_frames), 
-                                init_func=init, blit=True, repeat=False, 
-                                interval=interval_time)
-
-            plt.show()
-            # 将matplotlib动画转换为moviepy的VideoClip对象
-            duration = total_frames * (5000 / total_frames) / 1000.0
-            video_clip = VideoClip(lambda x: ani.to_rgba(x, bytes=True, norm=True), duration=duration)
-
-            # 保存为MP4
-            video_clip.write_videofile(f'{layer_dir}.mp4', fps=30)
-            plt.close()
-            # 如果你想要显示图例，可以使用以下命令：
-            # plt.legend()
-            # plt.show()
-            # plt.close()
             std_value_co1 = np.std(mfccs[0])
             print('std_value_co1:',std_value_co1)
             std_value_co2 = np.std(mfccs[1])
@@ -146,6 +90,8 @@ if os.path.exists(base_path):
             mean_value_co2 = np.mean(mfccs[1])
             mean_co1.append(mean_value_co1)
             mean_co2.append(mean_value_co2)  
+            # mean_mov_co1 = moving_average(mfccs[0],60)
+            # mean_mov_co2 = moving_average(mfccs[1],60)   
             window_length = int(mfccs.shape[1]/40)
             mean_mov_co1 = moving_average(mfccs[0],window_length)
             mean_mov_co2 = moving_average(mfccs[1],window_length)   
@@ -155,11 +101,12 @@ if os.path.exists(base_path):
             plt.plot(x_labels, mean_mov_co2, marker='o', linestyle='-',color="orange",label = 'mean_mov_co2')
             ax = plt.gca()  # 获取当前的axes对象
             ax.xaxis.set_major_locator(MaxNLocator(integer=True))  # 强制x轴刻度为整数
+            # plt.xlabel('Index of samples (60samples/per)')
             plt.xlabel(f'Index of samples ({window_length}samples/per)')
             plt.ylabel("MFCC coefficient mean_mov of")
             plt.title(f"Mean_mov of the MFCC of {layer_dir}")
             plt.legend()
-            # plt.show()
+            plt.show()
             plt.close()                  
             n += 1  
             # exit()
