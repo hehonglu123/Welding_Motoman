c48eff0b42821eb76c27c41a61d91d6c5c83a1c7
JInhan05013 <qhxnrjh@gmail.com>
2024-01-24 11:40:01 -0500

    Update weld_wall.py

--- weld/weld_wall.py
+++ weld/weld_wall.py
@@ -10,10 +10,14 @@ robot=robot_obj('MA2010_A0',def_path='../config/MA2010_A0_robot_default_config.y
 R=np.array([[-0.7071, 0.7071, -0.    ],
             [ 0.7071, 0.7071,  0.    ],
             [0.,      0.,     -1.    ]])
+# Top layers
 # p_start=np.array([1650,-840,-260])
 # p_end=np.array([1650,-780,-260])
+
+# Base Layers
 p_start=np.array([1650,-850,-260])
 p_end=np.array([1650,-770,-260])
+
 # p_start=np.array([1650,-860,-260])
 # p_end=np.array([1650,-760,-260])
 q_seed=np.radians([-35.4291,56.6333,40.5194,4.5177,-52.2505,-11.6546])
@@ -29,7 +33,8 @@ v_all=[]
 cond_all=[]
 primitives=[]
 
-for i in range(15,20):
+# Base layer
+for i in range(0,1):
 	if i%2==0:
 		p1=p_start+np.array([0,0,i*base_layer_height])
 		p2=p_end+np.array([0,0,i*base_layer_height])
@@ -51,6 +56,8 @@ for i in range(15,20):
 	primitives.extend(['movej','movel'])
 	cond_all.extend([0,int(feedrate/10)+200])
 
+# Top Layers
+	
 # for i in range(2,3):
 # 	if i%2==0:
 # 		p1=p_start+np.array([0,0,2*base_layer_height+i*layer_height])
@@ -61,6 +68,7 @@ for i in range(15,20):
 
 # 	q_init=robot.inv(p1,R,q_seed)[0]
 # 	q_end=robot.inv(p2,R,q_seed)[0]
+	
 # 	q_all.extend([q_init,q_end])
 # 	v_all.extend([1,15])
 # 	primitives.extend(['movej','movel'])
@@ -70,4 +78,4 @@ print('primitives',primitives)
 print('q_all',q_all)
 print('v_all',v_all)
 print('cond_all',cond_all)
-# ws.weld_segment_single(primitives,robot,q_all,v_all,cond_all,arc=False,wait=0.)
+ws.weld_segment_single(primitives,robot,q_all,v_all,cond_all,arc=False)
