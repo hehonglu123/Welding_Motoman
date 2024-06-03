2170334f10e62c285ed304e9eeb64a64109040c9
fusing-ubuntu <hehonglu123@126.com>
2024-06-03 17:09:16 -0400

    motoplus speed test

--- motoplus_rr/debug_speed.py
+++ motoplus_rr/debug_speed.py
@@ -4,20 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 sys.path.append('../toolbox/')
 from StreamingSend import *
+from robot_def import *
 
 
 ########################################################Robot CONFIG########################################################
-robot=robot_obj('MA2010_A0',def_path='../../config/MA2010_A0_robot_default_config.yml',tool_file_path='../../config/torch.csv',\
-	pulse2deg_file_path='../../config/MA2010_A0_pulse2deg_real.csv',d=15)
-robot2=robot_obj('MA1440_A0',def_path='../../config/MA1440_A0_robot_default_config.yml',tool_file_path='../../config/flir.csv',\
-		pulse2deg_file_path='../../config/MA1440_A0_pulse2deg_real.csv',base_transformation_file='../../config/MA1440_pose_mocap.csv')
-positioner=positioner_obj('D500B',def_path='../../config/D500B_robot_default_config.yml',tool_file_path='../../config/positioner_tcp.csv',\
-	pulse2deg_file_path='../../config/D500B_pulse2deg_real.csv',base_transformation_file='../../config/D500B_pose.csv')
+config_path='../config/'
+robot=robot_obj('MA2010_A0',def_path=config_path+'MA2010_A0_robot_default_config.yml',tool_file_path=config_path+'torch.csv',\
+	pulse2deg_file_path=config_path+'MA2010_A0_pulse2deg_real.csv',d=15)
+robot2=robot_obj('MA1440_A0',def_path=config_path+'MA1440_A0_robot_default_config.yml',tool_file_path=config_path+'flir.csv',\
+		pulse2deg_file_path=config_path+'MA1440_A0_pulse2deg_real.csv',base_transformation_file=config_path+'MA1440_pose_mocap.csv')
+positioner=positioner_obj('D500B',def_path=config_path+'D500B_robot_default_config.yml',tool_file_path=config_path+'positioner_tcp.csv',\
+	pulse2deg_file_path=config_path+'D500B_pulse2deg_real.csv',base_transformation_file=config_path+'D500B_pose.csv')
 
 ########################################################RR STREAMING########################################################
 
 # RR_robot_sub = RRN.SubscribeService('rr+tcp://192.168.55.10:59945?service=robot')
-RR_robot_sub = RRN.SubscribeService('rr+tcp://192.168.55.15:59945?service=robot')
+RR_robot_sub = RRN.SubscribeService('rr+tcp://localhost:59945?service=robot')
 RR_robot_state = RR_robot_sub.SubscribeWire('robot_state')
 RR_robot = RR_robot_sub.GetDefaultClientWait(1)
 robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", RR_robot)
@@ -49,16 +51,23 @@ curve_js=robot.find_curve_js(curve,[R]*num_points,q_seed)
 q_all=[]
 ts_all=[]
 
-SS.jog2q(np.hstack((curve_js[0],[0,0,0,0,0,0,np.radians(-15),np.pi])))
+SS.jog2q(np.hstack((curve_js[0],[np.pi/2,0,0,0,0,0,np.radians(-15),np.pi/2])))
+time.sleep(1)
 for i in range(num_points):
     point_stream_start_time=time.time()
-    robot_timestamp,q14=SS.position_cmd(np.hstack((curve_js[i],[0,0,0,0,0,0,np.radians(-15),np.pi])),time.time())
+    robot_timestamp,q14=SS.position_cmd(np.hstack((curve_js[i],[np.pi/2,0,0,0,0,0,np.radians(-15),np.pi/2])),time.time())
     q_all.append(q14)
     ts_all.append(robot_timestamp)
 
-p_all=robot.fwd(q_all).p_all
+q_all=np.array(q_all)
+ts_all=np.array(ts_all)
+p_all=robot.fwd(q_all[:,:6]).p_all
+np.savetxt('debug_speed_q_all.csv',np.hstack((ts_all[:,None],q_all[:,:6])),delimiter=',')
 #plot out velocity with ts_all timestamp
 v_all=np.linalg.norm(np.diff(p_all,axis=0),axis=1)/(np.diff(ts_all))
+#running average
+# v_all=np.convolve(v_all, np.ones((10,))/10, mode='valid')
+# ts_all=ts_all[5:-4]
 plt.plot(ts_all[1:],v_all)
 plt.show()
 
