4301ccf69961ce5d6d07bafde05b6e1a92080988
Chen-Lung Eric Lu <eric565648@gmail.com>
2024-12-13 14:09:17 -0500

    Create weld_wall.py

---
+++ weld/streaming_fuji/weld_wall.py
@@ -0,0 +1,15 @@
+import time, os, copy, sys
+import numpy as np
+from motoman_def import *
+from lambda_calc import *
+from RobotRaconteur.Client import *
+from weldRRSensor import *
+from StreamingSend import *
+from robotics_utils import *
+
+def main():
+    
+    weld_arcon = False
+
+if __name__ == '__main__':
+    main()
