9cd895e4e868235a244176c8ee588074e8c1cf74
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-12-13 16:46:53 -0500

    motion for fuji wall

--- weld/streaming_fuji/weld_wall.py
+++ weld/streaming_fuji/weld_wall.py
@@ -11,5 +11,18 @@ def main():
     
     weld_arcon = False
 
+    ############## Robot definition ##############
+    config_dir='../../config/'
+    robot=robot_obj('MA2010_A0',def_path=config_dir+'MA2010_A0_robot_default_config.yml',tool_file_path=config_dir+'torch.csv',\
+		pulse2deg_file_path=config_dir+'MA2010_A0_pulse2deg_real.csv',d=15)
+    positioner=positioner_obj('D500B',def_path=config_dir+'D500B_robot_extended_config.yml',tool_file_path=config_dir+'positioner_tcp.csv',\
+		pulse2deg_file_path=config_dir+'D500B_pulse2deg_real.csv',base_transformation_file=config_dir+'D500B_pose.csv')
+    
+    ########################################################RR STREAMING########################################################
+    # RR_robot_sub = RRN.SubscribeService('rr+tcp://192.168.55.12:59945?service=robot')
+    RR_robot_sub = RRN.SubscribeService('rr+tcp://localhost:59945?service=robot')
+    point_distance=0.04		###STREAMING POINT INTERPOLATED DISTANCE
+    SS=StreamingSend(RR_robot_sub,streaming_rate=125.)
+
 if __name__ == '__main__':
     main()
